056cb6642a97f22299d76e9143d7676068c2322d
Deni <deni@Tobis-MacBook-Pro.local>
2024-05-08 17:05:42 +0100

    update: FTU Dashboard-1

--- app/components/modals/index.tsx
+++ app/components/modals/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 interface IModal {
-  width?: string;
-  height?: string;
+  maxWidth?: string;
+  maxHeight?: string;
   closeModal: () => void;
   children: React.ReactNode;
 }
 
-function Modal({ width, height, closeModal, children }: IModal) {
-  width = width ?? "w-[802px]";
-  height = height ?? "h-[370px]";
+function Modal({ maxWidth, maxHeight, closeModal, children }: IModal) {
+  maxWidth = maxWidth ?? "max-w-[802px]";
+  maxHeight = maxHeight ?? "max-h-[370px]";
 
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 h-screen w-screen">
@@ -18,7 +18,7 @@ function Modal({ width, height, closeModal, children }: IModal) {
         onClick={closeModal}
       ></div>
       <div
-        className={`bg-white flex flex-col z-40 rounded-xl ${width} ${height} absolute left-[50%] 
+        className={`bg-white flex flex-col z-40 rounded-xl w-[80%] h-[80%] ${maxWidth} ${maxHeight} absolute left-[50%] 
       -translate-x-[50%] top-[50%] -translate-y-[50%]`}
       >
         {children}
